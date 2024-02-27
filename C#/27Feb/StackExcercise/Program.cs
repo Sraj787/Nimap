9fcc11865788bf3e29c2b01e5a66bc1e221f8c81
Sraj787 <137862674+Sraj787@users.noreply.github.com>
2024-02-27 00:09:10 -0800

    Add files via upload

---
+++ C#/27Feb/StackExcercise/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace StackExcercise
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            var  stack = new Stack();
+            int ini = 1;
+            int last = 5;
+
+            for(int i=ini;i<=last;i++)
+            {
+                stack.Push(i);
+            }
+
+            for (int i = ini; i <= last; i++)
+           {
+                Console.WriteLine(stack.Pop(i)); 
+            }
+
+            stack.Clear();
+
+            //stack is empty 
+            for (int i = ini; i <= last; i++)
+            {
+                Console.WriteLine(stack.Pop(i));
+            }
+        }
+    }
+}
---
+++ C#/27Feb/StackExcercise/Stack.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace StackExcercise
+{
+    internal partial class Program
+    {
+        public class Stack
+        {
+            private readonly List<object> _stack = new List<object>();
+
+            public void Push(Object obj)
+
+            {
+                if (obj == null)
+                    throw new InvalidOperationException("You can not null onject in stack.");
+                _stack.Add(obj);
+            }
+
+            public Object Pop(int count)
+            {
+                if (_stack.Count == 0)
+                    throw new InvalidOperationException("Stack is Empty.");
+                
+                var index = _stack.Count - 1;
+
+                var returnTo = _stack[index];
+                _stack.RemoveAt(index);
+
+                return returnTo;
+            }
+
+            public void Clear()
+            {
+                _stack.Clear();
+            }
+
+        }
+    }
+}
