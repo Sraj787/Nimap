12fe59d520191f71db74e6ed8f9f1f8bfdaf511e
Sraj787 <137862674+Sraj787@users.noreply.github.com>
2024-03-02 19:31:10 +0530

    Add files via upload

---
+++ C#/2March/CustomerController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers.api
+{
+    public class CustomerController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public CustomerController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return _context.Customer.ToList();
+        }
+
+        public Customer GetCustomersById(int id)
+        {
+            var customer = _context.Customer.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+            {
+                throw new HttpRequestException(HttpStatusCode.NotFound.ToString());
+            }
+
+            return customer;
+        }
+
+        [HttpPost]
+        public Customer CreateCustomers(Customer customers)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpRequestException(HttpStatusCode.BadRequest.ToString());
+
+            _context.Customer.Add(customers);
+            _context.SaveChanges();
+
+            return customers;
+        }
+        [HttpPut]
+        public Customer UpdateCustomer(int id, Customer customers)
+        {
+            var customerInDb = _context.Customer.SingleOrDefault(c => c.Id == id);
+            if (customerInDb == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            customerInDb.Id = customers.Id;
+            customerInDb.Name = customers.Name;
+            customerInDb.Address = customers.Address;
+            customerInDb.City = customers.City;
+
+            _context.Customer.Add(customers);
+            _context.SaveChanges();
+
+            return customers;
+        }
+        //Delete /api/customers/1
+        [HttpDelete]
+        public void DeleteCustomer(int id)
+        {
+            var customerInDb = _context.Customer.SingleOrDefault(c => c.Id == id);
+            if (customerInDb == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            _context.Customer.Remove(customerInDb);
+            _context.SaveChanges();
+
+        }
+    }
+}
