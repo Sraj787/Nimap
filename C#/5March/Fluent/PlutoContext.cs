2289eb6aea2e4d1df33a160bbceee7567953af9c
Sraj787 <137862674+Sraj787@users.noreply.github.com>
2024-03-05 12:50:59 +0530

    Add files via upload

---
+++ C#/5March/Fluent/PlutoContext.cs
@@ -0,0 +1,58 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace DataAnnotations
+{
+    public class PlutoContext : DbContext
+    {
+        public PlutoContext()
+            : base("name=PlutoContext")
+        {
+        }
+
+        public virtual DbSet<Author> Authors { get; set; }
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<Tag> Tags { get; set; }
+
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>()
+                .Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            modelBuilder.Entity<Course>()
+                .Property(c => c.Description)
+                .IsRequired()
+                .HasMaxLength(2000);
+
+            //One to Many Relationship Author with Course
+            modelBuilder.Entity<Course>()
+                .HasRequired(c => c.Author)
+                .WithMany(a => a.Courses)
+                .HasForeignKey(c => c.AuthorId)
+                .WillCascadeOnDelete(false);
+
+            //Many to Many RelationShip Course With Tags
+            modelBuilder.Entity<Course>()
+                .HasMany(c => c.Tags)
+                .WithMany(t => t.Courses)
+                .Map(m => 
+                {
+                    m.ToTable("CourseTags");
+                    m.MapLeftKey("CourseId");
+                    m.MapRightKey("TagId");
+         
+                });
+
+            //For One To One Realationship Between Course ANd Cover
+            modelBuilder.Entity<Course>()
+                .HasRequired(t => t.Cover)
+                .WithRequiredPrincipal(a => a.Course);
+        }
+
+
+    }
+}
