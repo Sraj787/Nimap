eefdbe87e2f24bb087b4f820c336582e1e1ae378
Sraj787 <137862674+Sraj787@users.noreply.github.com>
2024-03-08 10:24:49 +0530

    Add files via upload

---
+++ C#/7March/ViewModels/NewCustomerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ProboVideo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProboVideo.ViewModels
+{
+    public class NewCustomerViewModel
+    {
+        public IEnumerable<MemberShipTypes> MemberShipTypes { get; set; }
+
+        public Customer Customer { get; set; }
+    }
+}
---
+++ C#/7March/ViewModels/RandomModel.cs
@@ -0,0 +1,14 @@
+﻿using ProboVideo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProboVideo.ViewModels
+{
+    public class RandomModel
+    {
+        public Movie Movie { get; set; }
+        public IEnumerable<Genre> Genres{ get; set; }
+    }
+}
