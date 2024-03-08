2bf125ab7d0816ca3741124f8226590b4b8a8a06
Sraj787 <137862674+Sraj787@users.noreply.github.com>
2024-03-08 10:19:50 +0530

    Add files via upload

---
+++ C#/7March/Controllers/CustomerController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Ajax.Utilities;
+using ProboVideo.Models;
+using ProboVideo.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProboVideo.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        private ApplicationDbContext _context;
+        public CustomerController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new NewCustomerViewModel
+                {
+                    Customer = customer,
+                    MemberShipTypes = _context.MemberShipTypes.ToList()
+                };
+                return View("CustomerForm",viewModel);
+            }
+            if (customer.Id == 0)
+                _context.Customers.Add(customer);
+            else {
+                var customerDb = _context.Customers.Single(c=>c.Id== customer.Id);
+
+                customerDb.Name = customer.Name;
+                customerDb.Address = customer.Address;
+                customerDb.BirthDate = customer.BirthDate;
+                customerDb.MemberShipTypeId = customer.MemberShipTypeId;
+                customerDb.IsSubscribedToNowletter = customer.IsSubscribedToNowletter;
+                 }
+            
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customer");
+        }
+        // GET: Customer
+        public ActionResult Index()
+        {
+            //var customers = _context.Customers.ToString();
+            var customers = _context.Customers.Include("MemberShipType").ToList();
+            return View(customers);
+        }
+        
+        public ActionResult Details(int id)
+        {
+            var customer = _context.Customers.Include("MemberShipType").SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        public ActionResult New()
+        {
+            var membershipTypes = _context.MemberShipTypes.ToList();
+
+            var viewModel = new NewCustomerViewModel
+            {
+                Customer = new Customer(),
+                MemberShipTypes = membershipTypes
+            };
+            return View("CustomerForm",viewModel);
+        }
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c=>c.Id == id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            var viewModel = new NewCustomerViewModel { Customer = customer,
+                MemberShipTypes = _context.MemberShipTypes.ToList()
+            };
+            return View("CustomerForm", viewModel);
+        }
+    }
+}
---
+++ C#/7March/Controllers/MovieController.cs
@@ -0,0 +1,91 @@
+﻿using ProboVideo.Models;
+using ProboVideo.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProboVideo.Controllers
+{
+    public class MovieController : Controller
+    {
+
+        private ApplicationDbContext _context;
+
+        public MovieController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        // GET: Movie
+        public ActionResult Index()
+        {
+
+            var movie = _context.Movies.ToList();
+          /*  var movie = new Movie() { Title = "Shark!" };
+            var customer = new List<Customer>
+        {
+                new Customer{ Name="Customer 1"},
+                new Customer{ Name="Customer 2"},
+                new Customer{ Name="Customer 3"}
+            };
+
+            var viewModel = new RandomModel
+            {
+                Movie = movie,
+                Customer = customer
+            };*/
+
+            return View(movie);
+        }
+         public ActionResult Save(Movie movie) {
+
+            if(movie.Id==0)
+                _context.Movies.Add(movie);
+            else
+            {
+                var MovieDb = _context.Movies.SingleOrDefault(a=>a.Id==movie.Id);
+
+                MovieDb.Title = movie.Title;
+                MovieDb.Description = movie.Description;
+                MovieDb.AddedDate = movie.AddedDate;
+                MovieDb.ReleaseDate = movie.ReleaseDate;
+                MovieDb.NumberInStock = movie.NumberInStock;
+            }
+
+             _context.SaveChanges();
+
+            return View("Index","Movie");
+        }
+        public ActionResult New()
+        {
+            var moviegenre = _context.Genres.ToList();
+
+            var viewModel = new RandomModel
+            {
+                Movie = new Movie(),
+                Genres = moviegenre
+            };
+           return View("MovieForm",viewModel);
+        }
+        public ActionResult Edit(int id)
+        {
+            var movies = _context.Movies.SingleOrDefault(c => c.Id == id);
+            if (movies == null)
+            {
+                return HttpNotFound();
+            }
+            var viewModel = new RandomModel
+            {
+                Movie = new Movie(),
+                Genres = _context.Genres.ToList()
+            };
+            return View("MovieForm", viewModel);
+        }
+    }
+}
