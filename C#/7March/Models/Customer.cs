048b12443afe2f47d412b956dae0eb7f94aad6a9
Sraj787 <137862674+Sraj787@users.noreply.github.com>
2024-03-08 10:22:22 +0530

    Add files via upload

---
+++ C#/7March/Models/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProboVideo.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        [Min18YearCustomer]
+        public DateTime BirthDate { get; set; }
+        public bool IsSubscribedToNowletter { get; set; }
+
+        public MemberShipTypes memberShipType { get; set; }
+
+        public byte MemberShipTypeId { get; set; }
+    }
+}
---
+++ C#/7March/Models/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProboVideo.Models
+{
+    public class Movie
+    {
+
+        [Required]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public Genre genre { get; set; }
+
+        public byte genreId { get; set; }
+        public DateTime AddedDate { get; set;}
+
+        public DateTime ReleaseDate { get; set;}
+
+        public byte NumberInStock {  get; set;}
+
+    }
+}
