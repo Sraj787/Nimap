0fab095a852aee2fca6282b5f1b6bb6def9b31e0
Sraj787 <137862674+Sraj787@users.noreply.github.com>
2024-03-09 11:55:52 +0530

    Add files via upload

---
+++ C#/8March/api/CustomerController.cs
@@ -0,0 +1,76 @@
+﻿using AutoMapper;
+using ProboVideo.Dtos;
+using ProboVideo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProboVideo.Controllers.api
+{
+    public class CustomerController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public CustomerController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        [HttpPost]
+        public CustomerDto Create(CustomerDto customerdto)
+        {
+            var customer = Mapper.Map<CustomerDto, Customer>(customerdto);
+             _context.Customers.Add(customer);
+             _context.SaveChanges();
+
+            customerdto.Id = customer.Id;
+             return customerdto;
+        }
+        [HttpGet]
+        [Authorize(Roles = RolesName.ManageMovie)]
+        public IEnumerable <CustomerDto> Get()
+        {
+            var customers = _context.Customers.ToList().Select(Mapper.Map<Customer, CustomerDto>);
+
+            return customers;
+        }
+        [HttpGet]
+        public CustomerDto Get(int id)
+        {
+            return Mapper.Map<Customer,CustomerDto> (_context.Customers.Find(id));
+        }
+
+        [HttpPut]
+        [Authorize(Roles = RolesName.ManageMovie)]
+        public CustomerDto Update(int id,CustomerDto customerDto) 
+        {
+            var customerdb = _context.Customers.SingleOrDefault(m=>m.Id == id);
+
+
+            Mapper.Map(customerDto, customerdb);
+
+            _context.SaveChanges();
+
+            return customerDto;
+        }
+        [HttpDelete]
+        [Authorize(Roles = RolesName.ManageMovie)]
+        public string Delete(int id)
+        {
+            var deleteDb = _context.Customers.SingleOrDefault(m=>m.Id==id);
+
+            _context.Customers.Remove(deleteDb);
+
+            _context.SaveChanges();
+
+            return "Sucessfully Customer details Deleted";
+        }
+    }
+}
---
+++ C#/8March/api/MovieController.cs
@@ -0,0 +1,77 @@
+﻿using ProboVideo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProboVideo.Controllers.api
+{
+    public class MovieController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public MovieController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        [HttpGet]
+        [Authorize(Roles = RolesName.ManageMovie)]
+        public IEnumerable<Movie> Get()
+        {
+            var MovieALL = _context.Movies.ToList();
+
+            return MovieALL;
+        }
+        [HttpGet]
+        public Movie GetMovie(int id)
+        {
+            var movieByid = _context.Movies.FirstOrDefault(x => x.Id == id);
+
+            return movieByid;
+        }
+
+        [HttpPost]
+        [Authorize(Roles = RolesName.ManageMovie)]
+        public Movie Create(Movie movie)
+        {
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+            return movie;
+        }
+        [HttpPut]
+        [Authorize(Roles = RolesName.ManageMovie)]
+        public Movie Update(int id, Movie movie)
+        {
+            var movieDbModel = _context.Movies.FirstOrDefault(m=>m.Id == id);
+
+            movieDbModel.Title = movie.Title;
+            movieDbModel.Description = movie.Description;
+            movieDbModel.AddedDate = movie.AddedDate;
+            movieDbModel.ReleaseDate= movie.ReleaseDate;
+            movieDbModel.genreId = movie.genreId;
+            movieDbModel.NumberInStock = movie.NumberInStock;
+
+            _context.SaveChanges();
+
+            return movie;
+        }
+        [HttpDelete]
+        [Authorize(Roles = RolesName.ManageMovie)]
+        public string Delete(int id)
+        {
+            var deleteModel = _context.Movies.FirstOrDefault(m=>m.Id == id);
+
+            _context.Movies.Remove(deleteModel);
+            _context.SaveChanges();
+
+            return "SuccessFully Movie Details are deleted.";
+        }
+    }
+}
