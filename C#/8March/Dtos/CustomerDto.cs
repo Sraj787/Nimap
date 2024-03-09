f64cd48525ad5d0cb6bc578cda06b9a66e5ce93a
Sraj787 <137862674+Sraj787@users.noreply.github.com>
2024-03-09 11:57:05 +0530

    Add files via upload

---
+++ C#/8March/Dtos/CustomerDto.cs
@@ -0,0 +1,26 @@
+﻿using ProboVideo.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProboVideo.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        /*[Min18YearCustomer]*/
+        public DateTime BirthDate { get; set; }
+        public bool IsSubscribedToNowletter { get; set; }
+
+        public byte MemberShipTypeId { get; set; }
+    }
+}
