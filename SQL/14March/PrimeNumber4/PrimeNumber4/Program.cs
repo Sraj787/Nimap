718fe0aefe9b0bf473516f5b172e1c0404291e32
Sraj787 <137862674+Sraj787@users.noreply.github.com>
2024-03-14 19:38:01 +0530

    Add files via upload

---
+++ SQL/14March/PrimeNumber4/PrimeNumber4/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace PrimeNumber4
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int number = 8;
+            int temp = 0;
+            //Empty String
+            String primeNumbers = "";
+
+            for (int i = 1; i <= 100; i++)
+            {
+                int counter = 0;
+                for (int num = i; num >= 1; num--)
+                {
+                    if (i % num == 0)
+                    {
+                        counter = counter + 1;
+                    }
+                }
+                if (counter == 2)
+                {
+                    //Appended the Prime number to the String
+                    primeNumbers = primeNumbers + (i * i) + " ";
+                    temp++;
+                }
+                if (temp == number)
+                {
+                    break;
+                }
+            }           
+                Console.WriteLine("Prime numbers from 1 to 100 are :");
+                Console.WriteLine(primeNumbers);
+
+        }
+    }
+}
+    
+
---
+++ SQL/14March/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Security;
+
+namespace ArraySUM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 80, 10, 60, 50, 10, 20, 50 };
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i+1;j < array.Length; j++)
+                {
+                    int sum = 0;
+                    sum = array[i] + array[j];
+
+                    if(sum == 100)
+                    {
+                        Console.WriteLine(array[i]+ " " + array[j]+ "=" + sum);
+                    }
+                }
+            }
+        }
+    }
+}
